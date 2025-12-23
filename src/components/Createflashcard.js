99db73f31d0d64df73837a25c9d9d2907eb547c4
siddharth <siddharthsharma7678@gmail.com>
2025-12-23 18:24:40 +0530

    formik added to terms but not made perfectly working

--- src/components/Createflashcard.js
+++ src/components/Createflashcard.js
@@ -1,25 +1,31 @@
-import { useFormik } from "formik";
+import { Field, FieldArray, useFormik } from "formik";
 import React from "react";
 import { FaFileUpload } from "react-icons/fa";
 const Createflashcard = () => {
-
   const formik = useFormik({
-    initialValues:{
-      creategroup:"",
-      addDescription:"",
+    initialValues: {
+      creategroup: "",
+      addDescription: "",
+      groupImage: null,
+      Terms: [
+        {
+          term: "",
+          definition: "",
+          image: null,
+        },
+      ],
     },
-    onSubmit:values =>{
-      alert(JSON.stringify(values,null,2))
-    }
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
-
   // Append the Terms above the Terms using Add more button
-  const addTerms = (e) =>{
-      // const id = '';
-      const lowerTerm = document.querySelector('.TermMain');
-      const upperTerm = document.querySelector('#TermsContainer');
-      const child = upperTerm.cloneNode(true)
-      lowerTerm.appendChild(child);
+  const addTerms = (e) => {
+    // const id = '';
+    const lowerTerm = document.querySelector(".TermMain");
+    const upperTerm = document.querySelector("#TermsContainer");
+    const child = upperTerm.cloneNode(true);
+    lowerTerm.appendChild(child);
   };
   return (
     <>
@@ -49,6 +55,13 @@ const Createflashcard = () => {
               <input
                 id="fileInput"
                 type="file"
+                name="groupImage"
+                onChange={(event) =>
+                  formik.setFieldValue(
+                    "groupImage",
+                    event.currentTarget.files[0]
+                  )
+                }
                 className="hidden"
                 accept=".png, .svg, .jpg, .jpeg"
               />
@@ -56,10 +69,10 @@ const Createflashcard = () => {
             <div className="flex flex-col gap-2 col-span-2 m-4">
               <label className=" text-gray-400">Add description</label>
               <textarea
-              id="addDescription"
-              name="addDescription"
-              value={formik.values.addDescription}
-              onChange={formik.handleChange}
+                id="addDescription"
+                name="addDescription"
+                value={formik.values.addDescription}
+                onChange={formik.handleChange}
                 className="w-[40rem] h-28 border border-gray-300 rounded p-2"
                 placeholder="Enter description..."
               ></textarea>
@@ -68,41 +81,67 @@ const Createflashcard = () => {
         </form>
       </div>
       <div className="terms TermMain relative bg-white shadow-md mt-4 ml-24 w-3/4 border-2 rounded p-4">
-        <div id="TermsContainer" className="flex items-start gap-6">
-          {/* Term */}
-          <div className="flex flex-col">
-            <label className="mb-1 text-gray-400">
-              Enter Term*
-            </label>
-            <input
-              className="border border-gray-300 p-2 rounded w-64"
-              type="text"
-            />
-          </div>
+        <FieldArray name="terms">
+          {({ push, remove }) => {
+            <>
+              {formik.values.Terms.map((_, index) => {
+                <div id="TermsContainer" className="flex items-start gap-6">
+                  {/* Term */}
+                  <div className="flex flex-col">
+                    <label className="mb-1 text-gray-400">Enter Term*</label>
+                    <input
+                      key={index}
+                      name={`Terms[${index}].term`}
+                      value={formik.values.Terms[index].term}
+                      onChange={formik.handleChange}
+                      className="border border-gray-300 p-2 rounded w-64"
+                      type="text"
+                    />
+                  </div>
 
-          {/* Definition */}
-          <div className="flex flex-col">
-            <label className="mb-1 text-gray-400">Enter Definition*</label>
-            <textarea className="border border-gray-300 p-2 rounded w-80 h-20 resize-none"></textarea>
-          </div>
+                  {/* Definition */}
+                  <div className="flex flex-col">
+                    <label className="mb-1 text-gray-400">
+                      Enter Definition*
+                    </label>
+                    <textarea
+                      key={index}
+                      name={`Terms[${index}].definition`}
+                      onChange={formik.handleChange}
+                      value={formik.values.Terms[index].definition}
+                      className="border border-gray-300 p-2 rounded w-80 h-20 resize-none"
+                    ></textarea>
+                  </div>
 
-          {/* Image Upload */}
-          <div className="flex flex-col mt-12">
-            <label
-              htmlFor="fileInput1"
-              className="flex justify-center items-center gap-2 border border-gray-300 rounded w-36 h-10 cursor-pointer hover:border-blue-500"
-            >
-              <div className="text-blue-600">Select Image</div>
-            </label>
-            <input
-              id="fileInput1"
-              type="file"
-              className="hidden"
-              accept=".png, .svg, .jpg, .jpeg"
-            />
-          </div>
-        </div>
-        <h3 onClick={addTerms} className="text-blue-800 absolute bottom-1 left-2 cursor-pointer hover:text-blue-700">
+                  {/* Image Upload */}
+                  <div className="flex flex-col mt-12">
+                    <label
+                      htmlFor="fileInput1"
+                      className="flex justify-center items-center gap-2 border border-gray-300 rounded w-36 h-10 cursor-pointer hover:border-blue-500"
+                    >
+                      <div className="text-blue-600">Select Image</div>
+                    </label>
+                    <input
+                      onChange={(event) => {
+                        const file = event.currentTarget.files[0];
+                        setFieldValue(`Terms[${index}].image`, file);
+                      }}
+                      name={`Terms[${index}].image`}
+                      id="fileInput1"
+                      type="file"
+                      className="hidden"
+                      accept=".png, .svg, .jpg, .jpeg"
+                    />
+                  </div>
+                </div>;
+              })}
+            </>;
+          }}
+        </FieldArray>
+        <h3
+          onClick={addTerms}
+          className="text-blue-800 absolute bottom-1 left-2 cursor-pointer hover:text-blue-700"
+        >
           + Add more
         </h3>
         {/* <h3 onClick={addTerms} className="text-blue-800 absolute cursor-pointer hover:text-blue-700">
@@ -111,7 +150,9 @@ const Createflashcard = () => {
       </div>
       <div className="button flex justify-center items-center"></div>
       <div className="flex justify-center items-center m-4">
-        <button className="bg-red-600 text-white rounded p-1 w-24">Create</button>
+        <button className="bg-red-600 text-white rounded p-1 w-24">
+          Create
+        </button>
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,14 @@ const Navbar = () => {
         </nav>
 
         <div className="box mt-4 ml-24 h-24 w-3/4 border-b-2 border-gray-200">
-        <h2 className="font-bold text-xl">Create Flashcard</h2>
+          <h2 className="font-bold text-xl">Create Flashcard</h2>
           <div className="flex justify-start mt-4">
-            <div className="m-4 text-gray-400 border-b-2 rounded-sm p-[5px] border-red-500">Create New</div>
-            <div className="m-4 text-gray-400 border-b-2 rounded-sm p-[5px]">My Flashcard</div>
+            <div className="m-4 text-gray-400 border-b-2 rounded-sm p-[5px] border-red-500">
+              Create New
+            </div>
+            <div className="m-4 text-gray-400 border-b-2 rounded-sm p-[5px]">
+              My Flashcard
+            </div>
           </div>
         </div>
       </header>
